b987d12055aa464e14ede2e59a84d1b1a65e6baa
rohithyv <53818270+rohithyv@users.noreply.github.com>
2025-07-25 17:33:11 +0530

    added controllers and route file in bacend directory

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export function signup(req, res) {
+    res.send("Signup Route");
+}
+
+export function signup(req, res) {
+    res.send("Signup Route");
+}
+
+export function signup(req, res) {
+    res.send("Signup Route");
+}
+
+ 
